9b75338b8d4c43ac34c53d9dc99b2dc3e057732d
guifangqiang <m13721067940@163.com>
2021-01-20 18:34:12 +0800

    feat:添加启动加载逻辑

--- src/main/java/com/github/gfqlm/encryption/configuration/EncryptionConfiguration.java
+++ src/main/java/com/github/gfqlm/encryption/configuration/EncryptionConfiguration.java
@@ -1,8 +1,13 @@
 package com.github.gfqlm.encryption.configuration;
 
+import com.github.gfqlm.encryption.advice.EncryptionRequestAdvice;
+import com.github.gfqlm.encryption.advice.EncryptionResponseAdvice;
 import com.github.gfqlm.encryption.configuration.properties.EncryptionProperties;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
@@ -17,4 +22,17 @@ import org.springframework.stereotype.Component;
 @EnableAutoConfiguration
 @EnableConfigurationProperties(EncryptionProperties.class)
 public class EncryptionConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean(EncryptionRequestAdvice.class)
+    public EncryptionRequestAdvice getEncryptionRequestAdvice() {
+        return new EncryptionRequestAdvice();
+    }
+
+
+    @Bean
+    @ConditionalOnMissingBean(EncryptionResponseAdvice.class)
+    public EncryptionResponseAdvice getEncryptionResponseAdvice() {
+        return new EncryptionResponseAdvice();
+    }
 }
